72b53269f125b6011279a6b2c6db6fa8fd47a665
Eric Gonzalez <gonzerc@users.noreply.github.com>
2019-03-06 19:54:53 -0800

    Added character controller and moved camera to over the shoulder

---
+++ A Little Escape/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float rotationSpeed;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 movement = transform.forward * v * moveSpeed * Time.deltaTime;
+        rb.MovePosition(rb.position + movement);
+
+        float turn = h * rotationSpeed * Time.deltaTime;
+        Quaternion rotation = Quaternion.Euler(0f, turn, 0f);
+
+        rb.MoveRotation(rb.rotation * rotation);
+	}
+}
