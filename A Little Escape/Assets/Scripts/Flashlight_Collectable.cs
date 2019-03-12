9e2bd277eb09183124da16b4725de41888ae82fe
Jessica Ubaldo <jubaldo@csumb.edu>
2019-03-12 00:18:04 -0700

    Flashlight and Flashlight_Collectable

--- A Little Escape/Assets/Scripts/Flashlight_Collectable.cs
+++ A Little Escape/Assets/Scripts/Flashlight_Collectable.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Flashlight_Collectable : MonoBehaviour {
+    public GameObject FlashlightPlayer;
+
     void Start()
     {
-
     }
 
     void Update()
     {
-        transform.Rotate(new Vector3(0,5,0), 90.0f);
+        transform.Rotate(new Vector3(0.1f, 0.5f, 0.1f), 0.5f);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("First collision");
+        if (collision.collider.tag == "Player")
+        {
+            Debug.Log("Collision!");
+            // Enable FlashlightPlayer
+            FlashlightPlayer.gameObject.SetActive(true);
+
+            // destroy the flashlight
+            GameObject.Destroy(this.gameObject);
+        }
     }
 }
